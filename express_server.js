aa8141892f5a082e0310a365fe8f36cc97a8db8b
Chris Baltis <cbaltis@gmail.com>
2023-01-24 22:22:46 +0000

    added functionality to add username to header

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
 const PORT = 8080; // default port 8080
 function generateRandomString() {
   let sequence = '';
@@ -30,7 +32,7 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body);
+  res.cookie('username', req.body["username"]);
   res.redirect("/urls")
 })
 
@@ -57,16 +59,19 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {   username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
