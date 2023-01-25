398c09c642bca6fbc68f211630df2c0de1d25747
Chris Baltis <cbaltis@gmail.com>
2023-01-25 22:40:44 +0000

    added login and logout pages app in functional state

--- express_server.js
+++ express_server.js
@@ -13,8 +13,30 @@ function generateRandomString() {
   return sequence;
 }
 
+function getUserByEmail(givenEmail) {
+  for (const user in users) {
+    if (givenEmail === users[user].email) {
+      return users[user];
+    }
+  }
+  return;
+}
+
 
 app.set("view engine", "ejs");
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "123",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,13 +54,59 @@ app.post("/urls", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  res.cookie('username', req.body["username"]);
+  const email = req.body.email
+  const password = req.body.password
+  
+  
+  if (!email || !password) {
+    return res.send("400 Email or password should not be blank")
+  }
+
+  const user = getUserByEmail(email);
+  if (!user) {
+    return res.status(403).send("User not found")
+  }
+  if (user.password !== password) {
+    return res.status(403).send("Bad password")
+  }
+
+
+
+  // if (req.body['email'] === "" || req.body['password'] === "") {
+  //   res.send("400 Bad Request")
+  // }
+  // const person = validateUnusedEmail();
+  // if (validateUnusedEmail(req.body['email']) !== null) {
+  //   res.send("400 Bad Request")
+  // }
+  res.cookie('user_id', user.id);
+
+  // console.log(req.body.user_id["id"])
   res.redirect("/urls")
 })
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.body["username"]);
-  res.redirect("/urls")
+  res.clearCookie('user_id', req.body["user_id"]);
+  res.redirect("/login")
+})
+
+app.post("/register", (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  if (email === "" || password === "") {
+    res.status(400).send("Blank fields")
+    return;
+  }
+  if (getUserByEmail(email)) {
+    res.status(400).send("email registered already")
+    return;
+  }
+  const IDRandomizer = "user" + generateRandomString();
+  // console.log(IDRandomizer);
+  users[IDRandomizer] = {id: IDRandomizer, email: req.body["email"], password: req.body["password"]};
+  res.cookie('user_id', IDRandomizer);
+  // console.log(users);
+  res.redirect("/urls");
 })
 
 app.post("/urls/:id/edit", (req, res) => {
@@ -64,19 +132,39 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  const storeUserId = req.cookies["user_id"];
+  const templateVars = { user: users[storeUserId], urls: urlDatabase };
+  // console.log(users[storeUserId])
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/login", (req, res) => {
+  const storeUserId = req.cookies["user_id"];
   const templateVars = {
-    username: req.cookies["username"],
+    user: users[storeUserId],
+  };
+  res.render("urls_login", templateVars)
+})
+
+app.get("/register", (req, res) => {
+  const storeUserId = req.cookies["user_id"];
+  const templateVars = {
+    user: users[storeUserId],
+  };
+  res.render("urls_registration", templateVars)
+})
+
+app.get("/urls/new", (req, res) => {
+  const storeUserId = req.cookies["user_id"];
+  const templateVars = {
+    user: users[storeUserId]
   };
   res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = {   username: req.cookies["username"], id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const storeUserId = req.cookies["user_id"];
+  const templateVars = {   user: users[storeUserId], id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
